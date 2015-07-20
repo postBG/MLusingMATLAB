4ea0ab192222db83206ab09d7d3f5a3defc56756
unknown <profile2697@gmail.com>
2015-07-20 19:41:31 +0900

    Linear Regression with one variable is completed

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -13,9 +13,11 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+% X:m*2, y:m*1, theta:2*1
+hypho = X * theta;
+err = hypho - y;
+sq_of_error = err .^ 2;
+J = (1/(2*m))*sum(sq_of_error);
 
 % =========================================================================
 
--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,11 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
+    hypho = X * theta;
+    err = hypho - y; % m*1
+    theta = theta - alpha * (1/m) * (X' * err);
 
 
     % ============================================================
